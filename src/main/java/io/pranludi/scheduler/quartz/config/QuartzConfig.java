7b616fec64b8711bd6e87dd6a289bdabf0a3859e
daekwon <pranludi@gmail.com>
2025-04-09 16:35:10 +0900

    spring quartz 를 이용한 클러스팅 스케쥴러

--- src/main/java/io/pranludi/scheduler/quartz/config/QuartzConfig.java
+++ src/main/java/io/pranludi/scheduler/quartz/config/QuartzConfig.java
@@ -1,35 +1,114 @@
 package io.pranludi.scheduler.quartz.config;
 
 import io.pranludi.scheduler.quartz.job.SampleJob;
-import org.quartz.JobBuilder;
-import org.quartz.JobDetail;
-import org.quartz.SimpleScheduleBuilder;
+import java.io.IOException;
+import java.util.Properties;
 import org.quartz.Trigger;
-import org.quartz.TriggerBuilder;
+import org.springframework.beans.factory.config.PropertiesFactoryBean;
+import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.scheduling.quartz.JobDetailFactoryBean;
+import org.springframework.scheduling.quartz.SchedulerFactoryBean;
+import org.springframework.scheduling.quartz.SimpleTriggerFactoryBean;
 
 @Configuration
 public class QuartzConfig {
 
-    @Bean
-    public JobDetail sampleJobDetail() {
-        return JobBuilder.newJob(SampleJob.class)
-            .withIdentity("sampleJob")
-            .storeDurably()
-            .build();
+    private final ApplicationContext applicationContext;
+
+    public QuartzConfig(ApplicationContext applicationContext) {
+        this.applicationContext = applicationContext;
     }
 
+    // 스케줄링 설정
     @Bean
-    public Trigger sampleJobTrigger(JobDetail jobDetail) {
-        SimpleScheduleBuilder scheduleBuilder = SimpleScheduleBuilder.simpleSchedule()
-            .withIntervalInSeconds(20)
-            .repeatForever();
+    public SchedulerFactoryBean schedulerFactoryBean() {
+        SchedulerFactoryBean schedulerFactoryBean = new SchedulerFactoryBean();
 
-        return TriggerBuilder.newTrigger()
-            .forJob(jobDetail)
-            .withIdentity("sampleTrigger")
-            .withSchedule(scheduleBuilder)
-            .build();
+        // @Autowired 사용을 위한 설정
+        AutowiringSpringBeanJobFactory autowiringSpringBeanJobFactory = new AutowiringSpringBeanJobFactory();
+        autowiringSpringBeanJobFactory.setApplicationContext(applicationContext);
+        schedulerFactoryBean.setJobFactory(autowiringSpringBeanJobFactory);
+
+        // quartz scheduling 관련 설정
+        schedulerFactoryBean.setQuartzProperties(properties());
+
+        // 스케줄링을 위한 Trigger 등록
+        schedulerFactoryBean.setTriggers(
+            new Trigger[]{
+                simpleTriggerFactoryBean().getObject()
+            }
+        );
+
+        return schedulerFactoryBean;
     }
+
+    // JobDetail 설정
+    @Bean
+    public JobDetailFactoryBean jobDetailFactoryBean() {
+        JobDetailFactoryBean jobDetailFactoryBean = new JobDetailFactoryBean();
+        // 실행될 Job
+        jobDetailFactoryBean.setJobClass(SampleJob.class);
+        // 식별을 위한 Identity 등록 (group)
+        jobDetailFactoryBean.setGroup("SampleJobDetailGroup");
+        // 식별을 위한 Identity 등록 (name)
+        jobDetailFactoryBean.setName("SampleJobDetailName");
+        return jobDetailFactoryBean;
+    }
+
+    // Trigger 설정
+    @Bean
+    public SimpleTriggerFactoryBean simpleTriggerFactoryBean() {
+        SimpleTriggerFactoryBean simpleTriggerFactoryBean = new SimpleTriggerFactoryBean();
+        simpleTriggerFactoryBean.setJobDetail(jobDetailFactoryBean().getObject());
+        // 5초 간격으로 스케줄링 처리
+        simpleTriggerFactoryBean.setRepeatInterval(5000);
+        // 식별을 위한 Identity 등록 (group)
+        simpleTriggerFactoryBean.setGroup("SampleTriggerGroup");
+        // 식별을 위한 Identity 등록 (name)
+        simpleTriggerFactoryBean.setName("SampleTriggerName");
+        return simpleTriggerFactoryBean;
+    }
+
+//    @Bean
+//    public JobDetail sampleJobDetail() {
+//        return JobBuilder.newJob(SampleJob.class)
+//            .withIdentity("sampleJob")
+//            .storeDurably()
+//            .build();
+//    }
+
+//    @Bean
+//    public Trigger sampleJobTrigger(JobDetail jobDetail) {
+//        SimpleScheduleBuilder scheduleBuilder = SimpleScheduleBuilder.simpleSchedule()
+//            .withIntervalInSeconds(20)
+//            .repeatForever();
+//
+//        return TriggerBuilder.newTrigger()
+//            .forJob(jobDetail)
+//            .withIdentity("sampleTrigger")
+//            .withSchedule(scheduleBuilder)
+//            .build();
+//    }
+
+    // 스케줄링 Properties 설정
+    @Bean
+    public Properties properties() {
+        PropertiesFactoryBean propertiesFactoryBean = new PropertiesFactoryBean();
+        // quartz.properties 연결하기
+        propertiesFactoryBean.setLocation(new ClassPathResource("/quartz.properties"));
+        Properties properties = null;
+
+        try {
+            propertiesFactoryBean.afterPropertiesSet();
+            properties = propertiesFactoryBean.getObject();
+        } catch (IOException e) {
+            System.out.println("quartzProperties set error");
+        }
+
+        return properties;
+    }
+
 }
--- src/main/java/io/pranludi/scheduler/quartz/job/SampleJob.java
+++ src/main/java/io/pranludi/scheduler/quartz/job/SampleJob.java
@@ -7,14 +7,14 @@ import java.time.format.DateTimeFormatter;
 import org.quartz.Job;
 import org.quartz.JobExecutionContext;
 import org.quartz.JobExecutionException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
 
+@Component
 public class SampleJob implements Job {
 
-    private final LogJpaRepository logJpaRepository;
-
-    public SampleJob(LogJpaRepository logJpaRepository) {
-        this.logJpaRepository = logJpaRepository;
-    }
+    @Autowired
+    LogJpaRepository logJpaRepository;
 
     @Override
     public void execute(JobExecutionContext context) throws JobExecutionException {
