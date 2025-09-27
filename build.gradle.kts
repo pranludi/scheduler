9591854cc0f91f89a8976b318226db34b03b7a13
daekwon <pranludi@gmail.com>
2025-09-27 12:28:12 +0900

    wip

--- build.gradle.kts
+++ build.gradle.kts
@@ -21,7 +21,7 @@ dependencies {
   implementation("org.springframework.boot:spring-boot-starter-batch")
   implementation("org.springframework.boot:spring-boot-starter-data-jpa")
   implementation("org.springframework.boot:spring-boot-starter-quartz")
-  runtimeOnly("com.mysql:mysql-connector-j")
+  runtimeOnly("org.postgresql:postgresql:42.7.1")
   testImplementation("org.springframework.boot:spring-boot-starter-test")
   testRuntimeOnly("org.junit.platform:junit-platform-launcher")
 }
